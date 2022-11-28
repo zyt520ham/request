7014162014c7fd3cd6c0d6dc26f8a7d76301e60c
Soybean <honghuangdc@gmail.com>
2022-11-28 20:49:34 +0800

    update type

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,4 +1,52 @@
-import type { AxiosRequestConfig } from 'axios';
+import type { AxiosError, AxiosRequestConfig } from 'axios';
+
+/**
+ * 请求的错误类型：
+ * - axios: axios错误：网络错误, 请求超时, 默认的兜底错误
+ * - http: 请求成功，响应的http状态码非200的错误
+ * - backend: 请求成功，响应的http状态码为200，由后端定义的业务错误
+ */
+export type RequestErrorType = 'axios' | 'http' | 'backend';
+
+type AxiosErrorCode =
+  | 'ERR_FR_TOO_MANY_REDIRECTS'
+  | 'ERR_BAD_OPTION_VALUE'
+  | 'ERR_BAD_OPTION'
+  | 'ERR_NETWORK'
+  | 'ERR_DEPRECATED'
+  | 'ERR_BAD_RESPONSE'
+  | 'ERR_BAD_REQUEST'
+  | 'ERR_NOT_SUPPORT'
+  | 'ERR_INVALID_URL'
+  | 'ERR_CANCELED'
+  | 'ECONNABORTED'
+  | 'ETIMEDOUT';
+
+const BLANK_AXIOS_ERROR_CODE = '';
+
+/** 原始axios错误 */
+export type RawError<T, D> = Pick<AxiosError<T, D>, 'message' | 'config' | 'response'> & {
+  code: AxiosErrorCode | typeof BLANK_AXIOS_ERROR_CODE;
+  request: XMLHttpRequest;
+};
+
+/** 自定义错误 */
+export interface CustomError {
+  /** 请求服务的错误类型 */
+  type: RequestErrorType;
+  /** 错误码 */
+  code: string | number;
+  /** 错误信息 */
+  msg: string;
+}
+
+/** 请求错误 */
+export interface RequestError<T, D> {
+  /** 自定义的错误 */
+  error: CustomError;
+  /** 原始axios错误 */
+  rawError: RawError<T, D>;
+}
 
 /** 自定义的请求配置 */
 export interface RequestConfig {
